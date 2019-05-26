cc381b484128c7e35d4c023c9409c123f2ac08ef
Vasiliy Samoilichenko <vasiliy.samoilichenko@ewave.com>
2019-05-26 23:14:40 +0300

    changed start/down command sequence

--- down-devbox.sh
+++ down-devbox.sh
@@ -2,16 +2,10 @@
 set -eu
 #Variables
 source ./tools/main.sh
-source ./tools/install_dependency.sh
-source ./tools/devbox_infrastructure.sh
-source ./tools/env_file.sh
-source ./tools/free_port.sh
 source ./tools/web_platform.sh
+source ./tools/env_file.sh
 source ./tools/domain.sh
-source ./tools/restart_service.sh
-source ./tools/bash_alias.sh
-source ./tools/fix.sh
-source ./tools/print_info.sh
+source ./tools/devbox_infrastructure.sh
 
 #  Total: Run only one function
 stop_menu
